c6c0b4cc0ee303f4d92f66695935c53b80ec3914
Ergiz Gizer <ergizgizer@gmail.com>
2017-08-07 18:57:15 +0300

    Add gradle builds

--- app/src/main/java/com/ergizgizer/lasergame/LoginActivity.java
+++ app/src/main/java/com/ergizgizer/lasergame/LoginActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
