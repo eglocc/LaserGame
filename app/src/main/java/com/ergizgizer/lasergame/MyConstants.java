16b612ece3b694177165d7299cd789bec486f6c4
Ergiz Gizer <ergizgizer@gmail.com>
2017-08-07 18:53:39 +0300

    Initial commit

---
+++ app/src/main/java/com/ergizgizer/lasergame/MyConstants.java
@@ -0,0 +1,8 @@
+package com.ergizgizer.lasergame;
+
+public interface MyConstants {
+
+    String sAuthStateChanged = "onAuthStateChanged";
+    String sSignedIn = "signed_in";
+    String sSignedOut = "signed_out";
+}
